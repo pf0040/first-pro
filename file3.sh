bb18678a32a5818068683f7d13a777acb01e4fac
paul Sama <fosehpaul@gmail.com>
2021-08-19 11:30:12 -0500

    file3.sh created

---
+++ file3.sh
@@ -0,0 +1,9 @@
+print "Welcome to whatApp"
+print "What is your name?"
+read name
+print "Thanks for using whatApp $name"
+print "What is your pin?"
+read pin
+print "Are you sure Mr. $name that this is your pin $pin?"
+read remark
+print "Thanks for using whatApp"01~
